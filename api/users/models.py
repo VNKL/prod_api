61171e3cb8e946aa21bc1201d604254f6a139b96
VNKL <vnkl.iam@gmail.com>
2022-06-24 08:50:35 +0300

    change access_token max len

--- api/users/models.py
+++ api/users/models.py
@@ -6,7 +6,7 @@ class User(DjangoUser):
     DEFAULT_AVA_URL = 'http://my-engine.ru/modules/users/avatar.png'
 
     balance = models.IntegerField(default=0)
-    ads_token = models.CharField(max_length=100, blank=True, null=True)
+    ads_token = models.CharField(max_length=1000, blank=True, null=True)
     user_id = models.BigIntegerField(blank=True, null=True)
     ava_url = models.TextField(default=DEFAULT_AVA_URL)
     has_token = models.BooleanField(default=False)
